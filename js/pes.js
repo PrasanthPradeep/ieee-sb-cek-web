fa0f526b34aea2bf7adde3d49ff8e7b14d62a0cc
PrasanthPradeep <programmerprasanth@gmail.com>
2024-07-01 18:00:47 +0530

    first commit

---
+++ js/pes.js
@@ -0,0 +1,14 @@
+var setVanta = ()=>{
+  window.VANTA.BIRDS({
+  el: ".pes",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  colorMode: "lerp"
+})
+}
+setVanta()
